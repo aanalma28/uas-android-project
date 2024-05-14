c670043d7815a97db7c7185745cadc867d0b8e9f
Fikrianamm <fikritaufiqulanam1@gmail.com>
2024-05-14 10:01:08 +0700

    [fix] fix backend login

--- app/src/main/java/com/example/uasproject/LoginActivity.java
+++ app/src/main/java/com/example/uasproject/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.uasproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,9 +18,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private DatabaseHelper db;
     private EditText edt_email, edt_pass;
     private Button btn_login;
     private TextView to_register;
+
+    //shared pref
+    public static final String SHARED_PREF_NAME = "myPref";
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,46 @@ public class LoginActivity extends AppCompatActivity {
         edt_pass = findViewById(R.id.edt_pass);
         btn_login = findViewById(R.id.btn_login);
         to_register = findViewById(R.id.to_register);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        btn_login.setOnClickListener(v -> {
+            db = DatabaseHelper.getInstance(this);
+            try {
+
+                String email = edt_email.getText().toString();
+                String pass = edt_pass.getText().toString();
+
+                if (email.isEmpty() && pass.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "email atau password salah!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Boolean login = db.checkLogin(email, pass);
+                    if (login == true){
+                        Boolean updateSession = db.upgradeSession("ada", 1);
+                        if (updateSession == true){
+                            Toast.makeText(LoginActivity.this, "Berhasil masuk", Toast.LENGTH_SHORT).show();
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                            editor.putBoolean("masuk", true);
+                            editor.apply();
+                            Intent main = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(main);
+                            finish();
+                        }
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Gagal masuk", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }catch (Exception e){
+                Toast.makeText(LoginActivity.this, String.valueOf(e), Toast.LENGTH_SHORT).show();
+
+                Log.d("Debug App", "This is Debug");
+                Log.i("Debug App", "This is info");
+                Log.w("Debug App", "This is Warning");
+                Log.e("Debug Error", String.valueOf(e));
+            }
+        });
 
         to_register.setOnClickListener(
                 new View.OnClickListener() {
--- app/src/main/java/com/example/uasproject/MainActivity.java
+++ app/src/main/java/com/example/uasproject/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.uasproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_logout;
+
+    private DatabaseHelper db;
+
+    public static final String SHARED_PREF_NAME = "myPref";
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_logout = findViewById(R.id.btn_logout);
+
+        db = DatabaseHelper.getInstance(this);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        Boolean checksession = db.checkSession("ada");
+        if (checksession == false){
+            Intent login = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(login);
+            finish();
+        }
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Boolean updateSeesion = db.upgradeSession("kosong", 1);
+                if (updateSeesion == true){
+                    Toast.makeText(getApplicationContext(), "Berhasil Keluar", Toast.LENGTH_LONG).show();
+
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("masuk", false);
+                    editor.apply();
+
+                    Intent keluar = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(keluar);
+                    finish();
+                }
+            }
+        });
     }
 }
