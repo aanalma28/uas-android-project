ce7d44fe1334facc4ac341b83d468fae76fe213f
Fikri Taufiqul Anam <106758350+Fikrianamm@users.noreply.github.com>
2024-05-15 14:47:19 +0700

    [feat] menambahkan bottom nav n profile fragment  (#3)
    * [feat] menambahkan bottom nav dan profile fragment
    
    * [feat] change svg bottom nav n button logout in profile

--- app/src/main/java/com/example/uasproject/MainActivity.java
+++ app/src/main/java/com/example/uasproject/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.uasproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
-    private Button btn_logout;
+    private BottomNavigationView bottomNavigationView;
+    private HomeFragment homeFragment = new HomeFragment();
+    private PelajaranFragment pelajaranFragment = new PelajaranFragment();
+    private DashboardFragment dashboardFragment = new DashboardFragment();
+    private ProfileFragment profileFragment = new ProfileFragment();
     private FirebaseAuth mAuth;
     private DatabaseHelper db;
 
@@ -26,36 +35,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_logout = findViewById(R.id.btn_logout);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
         mAuth = FirebaseAuth.getInstance();
 
-//        db = DatabaseHelper.getInstance(this);
         sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
-//        Boolean checksession = db.checkSession("ada");
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser == null){
             Intent login = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(login);
             finish();
         }
+    }
 
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Boolean updateSeesion = db.upgradeSession("kosong", 1);
-                if (updateSeesion == true){
-                    Toast.makeText(getApplicationContext(), "Berhasil Keluar", Toast.LENGTH_LONG).show();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("masuk", false);
-                    editor.apply();
+        if (menuItem.getItemId() == R.id.home){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment, homeFragment).commit();
+            return true;
+        } else if (menuItem.getItemId() == R.id.pelajaran) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment, pelajaranFragment).commit();
+                return true;
+        }else if (menuItem.getItemId() == R.id.dashboard) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment, dashboardFragment).commit();
+                return true;
+        }else if (menuItem.getItemId() == R.id.profile) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragment, profileFragment).commit();
+                return true;
+        }
 
-                    Intent keluar = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(keluar);
-                    finish();
-                }
-            }
-        });
+        return false;
     }
 }
