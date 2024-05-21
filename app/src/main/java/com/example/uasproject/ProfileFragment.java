a40dbf0455f383903bb66749283854de6a7a1410
Fikrianamm <fikritaufiqulanam1@gmail.com>
2024-05-21 10:39:28 +0700

    [refactor] change save user data in shared preferences

--- app/src/main/java/com/example/uasproject/ProfileFragment.java
+++ app/src/main/java/com/example/uasproject/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.example.uasproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -8,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -27,33 +31,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class ProfileFragment extends Fragment {
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private DatabaseReference mDb = FirebaseDatabase.getInstance().getReference();
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    SharedPreferences sharedPreferences;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -62,10 +40,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -75,29 +49,26 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         TextView nama_akun = view.findViewById(R.id.nama_akun);
         TextView email_akun = view.findViewById(R.id.email_akun);
+        Button btn_logout = view.findViewById(R.id.logout);
+
+        sharedPreferences = getActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE);
+
+        btn_logout.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
+
+            startActivity(intent);
+            getActivity().finish();
+
+        });
 
         try{
-            DBFirebase getData = new DBFirebase();
-            FirebaseUser user = mAuth.getCurrentUser();
-            String id = user.getUid();
-            DatabaseReference data = getData.getUser(id);
-
-            data.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DataSnapshot> task) {
-                    if(!task.isSuccessful()){
-                        Log.e("firebase error", "Error getting data", task.getException());
-                    }else{
-                        String nama = task.getResult().child("name").getValue().toString();
-                        String email = task.getResult().child("email").getValue().toString();
-                        nama_akun.setText(nama);
-                        email_akun.setText(email);
-                        Log.d("firebase success", String.valueOf(task.getResult().getValue()));
-                    }
-                }
-            });
-
-            Log.d("Data success", "Success getting data");
+            String nama = sharedPreferences.getString("nama", "");
+            String email = sharedPreferences.getString("email", "");
+            nama_akun.setText(nama);
+            email_akun.setText(email);
         }catch(Exception e){
             Log.e("Data error", String.valueOf(e));
         }
