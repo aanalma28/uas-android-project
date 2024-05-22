6f01ed89e046d070752996fb7ba3d4f91e5b2914
Fikrianamm <fikritaufiqulanam1@gmail.com>
2024-05-22 23:27:39 +0700

    [feat] add layout create dan draft course

---
+++ app/src/main/java/com/example/uasproject/CreateCourseActivity.java
@@ -0,0 +1,24 @@
+package com.example.uasproject;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_course);
+        ImageView back = findViewById(R.id.back);
+        back.setOnClickListener(v -> {
+            finish();
+        });
+
+    }
+}
--- app/src/main/java/com/example/uasproject/DashboardActivity.java
+++ app/src/main/java/com/example/uasproject/DashboardActivity.java
@@ -1,6 +1,8 @@
 package com.example.uasproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,8 +18,18 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         ImageView back = findViewById(R.id.back);
+        Button createCourse = findViewById(R.id.btn_tambah_pelajaran);
+        Button draft = findViewById(R.id.btn_draft);
         back.setOnClickListener(v -> {
             finish();
+        });
+        createCourse.setOnClickListener(v -> {
+            Intent intent = new Intent(DashboardActivity.this, CreateCourseActivity.class);
+            startActivity(intent);
+        });
+        draft.setOnClickListener(v -> {
+            Intent intent = new Intent(DashboardActivity.this, DraftActivity.class);
+            startActivity(intent);
         });
     }
 }
